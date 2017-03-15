c28f5dfcb9ecd4c0ca2862370915a85b4fa8eb16
leer89 <leer89@gmail.com>
2017-03-15 10:49:21 -0400

    v1

--- project02-piglatin/Form1.cs
+++ project02-piglatin/Form1.cs
@@ -16,5 +16,15 @@ namespace project02_piglatin
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string englishText;
+            string [] english;
+            string [] pigLatin;
+
+            englishText = textBox1.Text;
+            english = englishText.Split(' ');
+        }
     }
 }
